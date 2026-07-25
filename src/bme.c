457a5b6f58f82cfaeceb00d51dfe0b74ee9a2d00
hasanpasha <bashahsn22@gmail.com>
2026-07-25 23:41:47 +0300

    improve `bme` CLI

--- src/bme.c
+++ src/bme.c
@@ -1,24 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <bm.h>
 
 Bm bm = {0};
 
+char *shift(int *argc, char ***argv) {
+  if (*argc < 1)
+    return NULL;
+  char *arg = **argv;
+  *argc -= 1;
+  *argv += 1;
+  return arg;
+}
+
+void usage(FILE *stream, const char *program) {
+  fprintf(stream, "Usage: %s <input.bm> [-h] [-l limit]\n", program);
+}
+
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    fprintf(stderr, "Error: usage: %s <input_file>\n", argv[0]);
+  const char *program = shift(&argc, &argv);
+
+  char *input_file = NULL;
+  int limit = -1;
+
+  char *arg = NULL;
+  while ((arg = shift(&argc, &argv)) != NULL) {
+    if (arg[0] == '-') {
+      char *flag = &arg[1];
+
+      if (strncmp(flag, "h", 1) == 0) {
+        usage(stdout, program);
+        return EXIT_SUCCESS;
+      } else if (strncmp(flag, "l", 1) == 0) {
+        char *limit_arg = shift(&argc, &argv);
+        if (limit_arg == NULL) {
+          usage(stderr, program);
+          fprintf(stderr, "Error: expected limit value.\n");
+          return EXIT_FAILURE;
+        }
+
+        limit = (int)strtol(limit_arg, NULL, 10);
+      } else {
+        usage(stderr, program);
+        fprintf(stderr, "Error: unknown flag '%s'\n", flag);
+        return EXIT_FAILURE;
+      }
+    } else {
+      if (input_file != NULL) {
+        usage(stderr, program);
+        fprintf(stderr, "Error: <input.bm> provided more than once.\n");
+        return EXIT_FAILURE;
+      }
+
+      input_file = arg;
+    }
+  }
+
+  if (input_file == NULL) {
+    usage(stderr, program);
+    fprintf(stderr, "Error: expected input.\n");
     return EXIT_FAILURE;
   }
 
-  char *input_file = argv[1];
   if (!bm_load_program_from_file(&bm, input_file)) {
     fprintf(stderr, "Error: failed to load program from '%s'.\n", input_file);
     return EXIT_FAILURE;
   }
 
-  BmError error;
-  if ((error = bm_execute_program(&bm, -1)) != BM_ERROR_OK) {
+  BmError error = bm_execute_program(&bm, limit);
+  if (error != BM_ERROR_OK) {
     fprintf(stderr, "Error: %s\n", bm_error_string(error));
     bm_stack_dump(&bm, stderr);
     fprintf(stderr, "\n");
