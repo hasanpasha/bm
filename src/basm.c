175b53089359d8e81ed72fdcf19054517fb1b765
hasanpasha <bashahsn22@gmail.com>
2026-07-25 23:41:54 +0300

    add comment to `basm`

--- src/basm.c
+++ src/basm.c
@@ -6,47 +6,50 @@
 static BmInst basm_translate_line(StringView line) {
   line = sv_ltrim(line);
   StringView inst_name = sv_chop_by_delim(&line, ' ');
+  StringView operand = sv_trim(sv_chop_by_delim(&line, '#'));
+
+  BmInst inst = {0};
 
   if (sv_eq(inst_name, sv_from_cstr("push"))) {
-    line = sv_ltrim(line);
-    BmWord operand = sv_parse_ulong(sv_rtrim(line));
-    return (BmInst){.type = BM_INST_TYPE_PUSH, .operand = operand};
+    inst =
+        (BmInst){.type = BM_INST_TYPE_PUSH, .operand = sv_parse_ulong(operand)};
   } else if (sv_eq(inst_name, sv_from_cstr("dup"))) {
-    line = sv_ltrim(line);
-    BmWord operand = sv_parse_ulong(sv_rtrim(line));
-    return (BmInst){.type = BM_INST_TYPE_DUP, .operand = operand};
+    inst =
+        (BmInst){.type = BM_INST_TYPE_DUP, .operand = sv_parse_ulong(operand)};
   } else if (sv_eq(inst_name, sv_from_cstr("jmp"))) {
-    line = sv_ltrim(line);
-    BmWord operand = sv_parse_ulong(sv_rtrim(line));
-    return (BmInst){.type = BM_INST_TYPE_JMP, .operand = operand};
+    inst =
+        (BmInst){.type = BM_INST_TYPE_JMP, .operand = sv_parse_ulong(operand)};
   } else if (sv_eq(inst_name, sv_from_cstr("jnz"))) {
-    line = sv_ltrim(line);
-    BmWord operand = sv_parse_ulong(sv_rtrim(line));
-    return (BmInst){.type = BM_INST_TYPE_JNZ, .operand = operand};
+    inst =
+        (BmInst){.type = BM_INST_TYPE_JNZ, .operand = sv_parse_ulong(operand)};
   } else if (sv_eq(inst_name, sv_from_cstr("plus"))) {
-    return (BmInst){.type = BM_INST_TYPE_PLUS};
+    inst = (BmInst){.type = BM_INST_TYPE_PLUS};
   } else if (sv_eq(inst_name, sv_from_cstr("dump"))) {
-    return (BmInst){.type = BM_INST_TYPE_DUMP};
+    inst = (BmInst){.type = BM_INST_TYPE_DUMP};
   } else if (sv_eq(inst_name, sv_from_cstr("teq"))) {
-    return (BmInst){.type = BM_INST_TYPE_TEQ};
+    inst = (BmInst){.type = BM_INST_TYPE_TEQ};
   } else if (sv_eq(inst_name, sv_from_cstr("hlt"))) {
-    return (BmInst){.type = BM_INST_TYPE_HLT};
+    inst = (BmInst){.type = BM_INST_TYPE_HLT};
   } else {
     fprintf(stderr, "Error: unknown instruction '" SV "'\n", SV_ARG(inst_name));
     exit(1);
   }
 
-  return (BmInst){0};
+  return inst;
 }
 
 static size_t basm_translate_source(StringView source, BmInst *program,
                                     size_t program_cap) {
 
   size_t program_size = 0;
-  while (source.len > 0 && program_size < program_cap) {
+  while (!sv_is_blank(source) && program_size < program_cap) {
     StringView line = sv_trim(sv_chop_by_delim(&source, '\n'));
     if (!sv_is_blank(line)) {
-      program[program_size++] = basm_translate_line(line);
+      if (sv_begins_with(line, sv_from_cstr("#"))) {
+        continue;
+      } else {
+        program[program_size++] = basm_translate_line(line);
+      }
     }
   }
 
