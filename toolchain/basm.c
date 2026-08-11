69f6ffaa18162a03f76a84415fb6d2114c4493b1
Hasan Pasha <bashahsn22@gmail.com>
2026-08-11 19:41:01 +0300

    update basm for proper instr operand parse

--- toolchain/basm.c
+++ toolchain/basm.c
@@ -8,15 +8,20 @@
 #define STRING_VIEW_IMPLEMENTATION
 #include <string_view.h>
 
+#include "basm/ast.h"
+#include "basm/lexer.h"
+#include "basm/parser.h"
+#include "basm/token.h" fprint
+
 #define BASM_LABELS_CAP 1024
 #define BASM_DEFERRED_OPERANDS_CAP 1024
 
 typedef struct BASM_LABEL {
   StringView name;
-  BmInstAddr addr;
+  BasmExpression value;
 } BasmLabel;
 
-typedef struct BASM_UNRESOLVED_JMP {
+typedef struct BASM_DEFERRED_OPERAND {
   BmInstAddr addr;
   StringView label;
 } BasmDeferredOperand;
@@ -29,23 +34,24 @@ typedef struct BASM {
   size_t deferred_operands_len;
 } Basm;
 
-static uint64_t basm_find_label(const Basm *basm, StringView name) {
+static bool basm_find_label(const Basm *basm, StringView name,
+                            BasmExpression *value) {
   for (size_t i = 0; i < basm->labels_len; i++) {
     BasmLabel label = basm->labels[i];
     if (sv_eq(label.name, name)) {
-      return label.addr;
+      if (value != NULL) {
+        *value = label.value;
+      }
+      return true;
     }
   }
 
-  PANIC("failed to resolve label '" SV_FMT "'.", SV_ARG(name));
+  return false;
 }
 
-static void basm_push_label(Basm *basm, StringView name, BmInstAddr addr) {
+static void basm_push_value(Basm *basm, StringView name, BasmExpression value) {
   assert(basm->labels_len < BASM_LABELS_CAP);
-  basm->labels[basm->labels_len++] = (BasmLabel){
-      .name = name,
-      .addr = addr,
-  };
+  basm->labels[basm->labels_len++] = (BasmLabel){.name = name, .value = value};
 }
 
 static void basm_push_deferred_operand(Basm *basm, BmInstAddr addr,
@@ -58,120 +64,183 @@ static void basm_push_deferred_operand(Basm *basm, BmInstAddr addr,
       };
 }
 
-static BmWord basm_parse_word(StringView sv) {
-  sv = sv_trim(sv);
-  BmWord word;
-  if (sv_chop_right(&sv, sv_from_cstr("u64"))) {
-    word.u64 = sv_parse_ulong(sv);
-  } else if (sv_chop_right(&sv, sv_from_cstr("i64"))) {
-    word.i64 = sv_parse_long(sv);
-  } else if (sv_chop_right(&sv, sv_from_cstr("f64"))) {
-    word.f64 = sv_parse_double(sv);
-  } else {
-    word.u64 = sv_parse_ulong(sv);
+static BasmExpression basm_fold_expr(Basm *basm, BasmExpression expr) {
+  switch (expr.kind) {
+  case BASM_EXPRESSION_KIND_NONE:
+  case BASM_EXPRESSION_KIND_INTEGER:
+  case BASM_EXPRESSION_KIND_FLOAT:
+    break;
+  case BASM_EXPRESSION_KIND_VARIABLE: {
+    BasmExpression value;
+    if (basm_find_label(basm, expr.u.variable.lexeme, &value)) {
+      expr = value;
+    } else {
+      basm_push_deferred_operand(basm, basm->prg.len, expr.u.variable.lexeme);
+    }
+  } break;
+  case BASM_EXPRESSION_KIND_UNARY: {
+    BasmExpression operand_value = basm_fold_expr(basm, *expr.u.unary.operand);
+    switch (operand_value.kind) {
+    case BASM_EXPRESSION_KIND_INTEGER: {
+      expr.kind = operand_value.kind;
+      switch (expr.u.unary.operator) {
+      case BASM_EXPRESSION_UNARY_OPERATOR_MINUS:
+        expr.u.integer = -operand_value.u.integer;
+        break;
+      default:
+        break;
+      }
+    } break;
+    case BASM_EXPRESSION_KIND_FLOAT: {
+      expr.kind = operand_value.kind;
+      switch (expr.u.unary.operator) {
+      case BASM_EXPRESSION_UNARY_OPERATOR_MINUS:
+        expr.u._float = -operand_value.u._float;
+        break;
+      default:
+        break;
+      }
+    } break;
+    case BASM_EXPRESSION_KIND_NONE:
+    case BASM_EXPRESSION_KIND_VARIABLE:
+    case BASM_EXPRESSION_KIND_UNARY:
+      *expr.u.unary.operand = operand_value;
+      break;
+    default:
+      BM_UNREACHABLE();
+    }
+  } break;
+  default:
+    break;
   }
+
+  return expr;
+}
+
+static BmWord basm_parse_word(Basm *basm, BasmExpression expr, bool force) {
+  (void)basm;
+
+  expr = basm_fold_expr(basm, expr);
+
+  BmWord word = {0};
+
+  switch (expr.kind) {
+  case BASM_EXPRESSION_KIND_NONE:
+    PANIC("can't resolve none expression");
+    break;
+  case BASM_EXPRESSION_KIND_INTEGER:
+    word.u64 = expr.u.integer;
+    break;
+  case BASM_EXPRESSION_KIND_FLOAT:
+    word.f64 = expr.u._float;
+    break;
+  case BASM_EXPRESSION_KIND_VARIABLE:
+  case BASM_EXPRESSION_KIND_UNARY:
+    if (force)
+      PANIC("can't resolve expr");
+    break;
+  default:
+    BM_UNREACHABLE();
+  }
+
   return word;
 }
 
-static void basm_translate_source(Basm *basm, StringView source) {
-  basm->prg.len = 0;
-  while (!sv_is_blank(source)) {
-    assert(basm->prg.len < BM_PROGRAM_CAP);
+#define TRY_MATCH_NO_OPERAND_INST(inst_type)                                   \
+  if (sv_eq(basm_inst.inst_name.lexeme,                                        \
+            sv_from_cstr(bm_inst_type_readable_name(inst_type))))              \
+    inst.type = inst_type;
 
-    StringView line = sv_trim(sv_chop_by_delim(&source, '\n'));
+static bool basm_assemble_file(Basm *basm, const char *input_path,
+                               const char *output_path) {
+  StringView source = sv_read_file(input_path);
 
-    if (sv_is_blank(line))
-      continue;
+  BasmLexer lexer = basm_lexer_init(source);
+  BasmParser parser = basm_parser_init(lexer);
 
-    if (sv_begins_with(line, sv_from_cstr("#")))
-      continue;
+  BasmStatement stmt;
+  while (basm_parser_statement(&parser, &stmt)) {
+    if (stmt.kind != BASM_STATEMENT_KIND_INSTRUCTION)
+      PANIC("only instruction statement are supported at the moment");
 
-    line = sv_ltrim(line);
-    StringView inst_name = sv_chop_by_delim(&line, ' ');
+    BasmInst basm_inst = stmt.u.inst;
 
-    if (sv_chop_right(&inst_name, sv_from_cstr(":"))) {
-      basm_push_label(basm, inst_name, basm->prg.len);
-
-      line = sv_ltrim(line);
-      inst_name = sv_chop_by_delim(&line, ' ');
+    if (!sv_is_blank(basm_inst.label.lexeme)) {
+      basm_push_value(basm, basm_inst.label.lexeme,
+                      (BasmExpression){.kind = BASM_EXPRESSION_KIND_INTEGER,
+                                       .u.integer = basm->prg.len});
     }
 
-    if (sv_is_blank(inst_name))
-      continue;
-
-    StringView operand = sv_trim(sv_chop_by_delim(&line, '#'));
-
-#define TRY_PARSE_NO_OP_INST(bm_type)                                          \
-  if (sv_eq(inst_name, sv_from_cstr(bm_inst_type_readable_name(bm_type))))     \
-    inst.type = bm_type;
-
-#define TRY_PARSE_ULONG_OP_INST(bm_type)                                       \
-  if (sv_eq(inst_name, sv_from_cstr(bm_inst_type_readable_name(bm_type)))) {   \
-    inst.type = bm_type;                                                       \
-    inst.operand.u64 = sv_parse_ulong(operand);                                \
-  }
-
-#define TRY_PARSE_WORD_OP_INST(bm_type)                                        \
-  if (sv_eq(inst_name, sv_from_cstr(bm_inst_type_readable_name(bm_type)))) {   \
-    inst.type = bm_type;                                                       \
-    inst.operand = basm_parse_word(operand);                                   \
-  }
-
-#define TRY_PARSE_ULONG_OR_LABEL_OP_INST(bm_type)                              \
-  if (sv_eq(inst_name, sv_from_cstr(bm_inst_type_readable_name(bm_type)))) {   \
-    inst.type = bm_type;                                                       \
-    if (operand.len > 0 && isdigit((unsigned char)operand.ptr[0]))             \
-      inst.operand.u64 = sv_parse_ulong(operand);                              \
-    else                                                                       \
-      basm_push_deferred_operand(basm, basm->prg.len, operand);                \
-  }
-
     BmInst inst = {0};
-    TRY_PARSE_NO_OP_INST(BM_INST_TYPE_NO_OPERATION)                       //
-    else TRY_PARSE_WORD_OP_INST(BM_INST_TYPE_PUSH)                        //
-        else TRY_PARSE_ULONG_OP_INST(BM_INST_TYPE_DUPLICATE)              //
-        else TRY_PARSE_ULONG_OR_LABEL_OP_INST(BM_INST_TYPE_JUMP)          //
-        else TRY_PARSE_ULONG_OR_LABEL_OP_INST(BM_INST_TYPE_CALL)          //
-        else TRY_PARSE_ULONG_OR_LABEL_OP_INST(BM_INST_TYPE_JMP_IF_TRUE)   //
-        else TRY_PARSE_ULONG_OP_INST(BM_INST_TYPE_SWAP)                   //
-        else TRY_PARSE_ULONG_OP_INST(BM_INST_TYPE_NATIVE)                 //
-        else TRY_PARSE_NO_OP_INST(BM_INST_TYPE_DROP)                      //
-        else TRY_PARSE_NO_OP_INST(BM_INST_TYPE_ADD_INT)                   //
-        else TRY_PARSE_NO_OP_INST(BM_INST_TYPE_SUBTRACT_INT)              //
-        else TRY_PARSE_NO_OP_INST(BM_INST_TYPE_MULTIPLY_INT)              //
-        else TRY_PARSE_NO_OP_INST(BM_INST_TYPE_DIVIDE_INT)                //
-        else TRY_PARSE_NO_OP_INST(BM_INST_TYPE_ADD_FLOAT)                 //
-        else TRY_PARSE_NO_OP_INST(BM_INST_TYPE_SUBTRACT_FLOAT)            //
-        else TRY_PARSE_NO_OP_INST(BM_INST_TYPE_MULTIPLY_FLOAT)            //
-        else TRY_PARSE_NO_OP_INST(BM_INST_TYPE_DIVIDE_FLOAT)              //
-        else TRY_PARSE_NO_OP_INST(BM_INST_TYPE_TEST_EQUALS)               //
-        else TRY_PARSE_NO_OP_INST(BM_INST_TYPE_HALT)                      //
-        else TRY_PARSE_NO_OP_INST(BM_INST_TYPE_NOT)                       //
-        else TRY_PARSE_NO_OP_INST(BM_INST_TYPE_TEST_GREATER_EQUALS_FLOAT) //
-        else TRY_PARSE_NO_OP_INST(BM_INST_TYPE_RETURN)                    //
-        else PANIC("unknown instruction '" SV_FMT "'.", SV_ARG(inst_name));
+    bool found = false;
+    for (size_t i = 0; i < BM_NUM_OF_INST_TYPES; i++) {
+      BmInstType type = (BmInstType)i;
+
+      if (!bm_inst_type_has_operand(type) &&
+          sv_eq(basm_inst.inst_name.lexeme,
+                sv_from_cstr(bm_inst_type_readable_name(type)))) {
+        inst.type = type;
+        found = true;
+        break;
+      }
+    }
+
+    if (found) {
+      bm_program_push(&basm->prg, inst);
+      continue;
+    }
+
+    if (sv_eq(sv_from_cstr(bm_inst_type_readable_name(BM_INST_TYPE_PUSH)),
+              basm_inst.inst_name.lexeme)) {
+      inst.type = BM_INST_TYPE_PUSH;
+      inst.operand = basm_parse_word(basm, basm_inst.expr, false);
+    } else if (sv_eq(
+                   sv_from_cstr(bm_inst_type_readable_name(BM_INST_TYPE_JUMP)),
+                   basm_inst.inst_name.lexeme)) {
+      inst.type = BM_INST_TYPE_JUMP;
+      inst.operand = basm_parse_word(basm, basm_inst.expr, false);
+    } else if (sv_eq(sv_from_cstr(
+                         bm_inst_type_readable_name(BM_INST_TYPE_JMP_IF_TRUE)),
+                     basm_inst.inst_name.lexeme)) {
+      inst.type = BM_INST_TYPE_JMP_IF_TRUE;
+      inst.operand = basm_parse_word(basm, basm_inst.expr, false);
+    } else if (sv_eq(
+                   sv_from_cstr(bm_inst_type_readable_name(BM_INST_TYPE_CALL)),
+                   basm_inst.inst_name.lexeme)) {
+      inst.type = BM_INST_TYPE_CALL;
+      inst.operand = basm_parse_word(basm, basm_inst.expr, false);
+    } else if (sv_eq(sv_from_cstr(
+                         bm_inst_type_readable_name(BM_INST_TYPE_DUPLICATE)),
+                     basm_inst.inst_name.lexeme)) {
+      inst.type = BM_INST_TYPE_DUPLICATE;
+      inst.operand = basm_parse_word(basm, basm_inst.expr, false);
+    } else if (sv_eq(
+                   sv_from_cstr(bm_inst_type_readable_name(BM_INST_TYPE_SWAP)),
+                   basm_inst.inst_name.lexeme)) {
+      inst.type = BM_INST_TYPE_SWAP;
+      inst.operand = basm_parse_word(basm, basm_inst.expr, false);
+    } else if (sv_eq(sv_from_cstr(
+                         bm_inst_type_readable_name(BM_INST_TYPE_NATIVE)),
+                     basm_inst.inst_name.lexeme)) {
+      inst.type = BM_INST_TYPE_NATIVE;
+      inst.operand = basm_parse_word(basm, basm_inst.expr, false);
+    } else {
+      PANIC("unknown inst name '" SV_FMT "'",
+            SV_ARG(basm_inst.inst_name.lexeme));
+    }
 
     bm_program_push(&basm->prg, inst);
   }
 
   for (size_t i = 0; i < basm->deferred_operands_len; i++) {
     BasmDeferredOperand jmp = basm->deferred_operands[i];
-    uint64_t addr = basm_find_label(basm, jmp.label);
-    basm->prg.ptr[jmp.addr].operand.u64 = addr;
+    BasmExpression value;
+    if (!basm_find_label(basm, jmp.label, &value))
+      PANIC("failed to resolve '" SV_FMT "'.", SV_ARG(jmp.label));
+    basm->prg.ptr[jmp.addr].operand = basm_parse_word(basm, value, true);
   }
 
-#undef TRY_PARSE_NO_OP_INST
-#undef TRY_PARSE_ULONG_OP_INST
-#undef TRY_PARSE_WORD_OP_INST
-#undef TRY_PARSE_ULONG_OR_LABEL_OP_INST
-}
-
-static bool basm_assemble_file(Basm *basm, const char *input_path,
-                               const char *output_path) {
-  StringView sv = sv_read_file(input_path);
-  basm_translate_source(basm, sv);
-  free((void *)sv.ptr);
-
+  free((void *)source.ptr);
   return bm_program_save_to_file(&basm->prg, output_path);
 }
 
